625648d1bfcb876284dd2d459344c9929c4bfea3
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-01-26 19:37:53 -0800

    Simplify method calls

--- src/dg-scan.c
+++ src/dg-scan.c
@@ -5,9 +5,9 @@
 int main() {
   struct Program dg_scan;
   Program_init(&dg_scan);
-  (dg_scan.setName)(&dg_scan, "DG Scan");
-  (dg_scan.setVersion)(&dg_scan, "1.0");
-  (dg_scan.setAuthor)(&dg_scan, "Daniel Gilbert");
-  (dg_scan.printBanner)(&dg_scan);
+  dg_scan.setName(&dg_scan, "DG Scan");
+  dg_scan.setVersion(&dg_scan, "1.0");
+  dg_scan.setAuthor(&dg_scan, "Daniel Gilbert");
+  dg_scan.printBanner(&dg_scan);
   return 0;
 }
