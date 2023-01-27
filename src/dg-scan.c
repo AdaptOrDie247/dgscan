2e31f449d805c19c58994b9091004f8cfadb4079
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-01-26 21:10:15 -0800

    Alphabetize macros and function calls

--- src/dg-scan.c
+++ src/dg-scan.c
@@ -2,10 +2,10 @@
 #include "program.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define PROGRAM_NAME "DG Scan"
-#define PROGRAM_BINARY_NAME "dg-scan"
-#define PROGRAM_VERSION "1.0"
 #define PROGRAM_AUTHOR "Daniel Gilbert"
+#define PROGRAM_BINARY_NAME "dg-scan"
+#define PROGRAM_NAME "DG Scan"
+#define PROGRAM_VERSION "1.0"
 int main(void) {
   Program dg_scan;
   Main_initProgram(&dg_scan);
@@ -15,8 +15,8 @@ int main(void) {
 }
 void Main_initProgram(Program* program) {
   Program_init(program);
-  program->setName(program, PROGRAM_NAME);
-  program->setBinaryName(program, PROGRAM_BINARY_NAME);
-  program->setVersion(program, PROGRAM_VERSION);
   program->setAuthor(program, PROGRAM_AUTHOR);
+  program->setBinaryName(program, PROGRAM_BINARY_NAME);
+  program->setName(program, PROGRAM_NAME);
+  program->setVersion(program, PROGRAM_VERSION);
 }
