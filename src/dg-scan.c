e36d47422b807faaa5933150cd68dc4331057e4f
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-01-26 20:19:16 -0800

    Move Program initialization into main

--- src/dg-scan.c
+++ src/dg-scan.c
@@ -6,14 +6,14 @@
 #define PROGRAM_VERSION "1.0"
 #define PROGRAM_AUTHOR "Daniel Gilbert"
 int main() {
-  Main_printBanner();
-  return 0;
-}
-void Main_printBanner() {
   struct Program dg_scan;
   Program_init(&dg_scan);
-  dg_scan.setName(&dg_scan, PROGRAM_NAME);
-  dg_scan.setVersion(&dg_scan, PROGRAM_VERSION);
-  dg_scan.setAuthor(&dg_scan, PROGRAM_AUTHOR);
-  dg_scan.printBanner(&dg_scan);
+  Main_printBanner(&dg_scan);
+  return 0;
+}
+void Main_printBanner(struct Program* program) {
+  program->setName(program, PROGRAM_NAME);
+  program->setVersion(program, PROGRAM_VERSION);
+  program->setAuthor(program, PROGRAM_AUTHOR);
+  program->printBanner(program);
 }
