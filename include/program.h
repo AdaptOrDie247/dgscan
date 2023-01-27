864be541c29238273ebf20c51c10fc647eeeae44
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-01-26 19:40:59 -0800

    Fix parameter name

--- include/program.h
+++ include/program.h
@@ -12,12 +12,12 @@ struct Program {
   void (*setName)(void* self, char* name);
   void (*setVersion)(void* self, char* version);
 };
-int Program_init(struct Program* program);
-char* Program_getAuthor(struct Program* program);
-char* Program_getName(struct Program* program);
-char* Program_getVersion(struct Program* program);
-void Program_printBanner(struct Program* program);
-void Program_setAuthor(struct Program* program, char* author);
-void Program_setName(struct Program* program, char* name);
-void Program_setVersion(struct Program* program, char* version);
+int Program_init(struct Program* self);
+char* Program_getAuthor(struct Program* self);
+char* Program_getName(struct Program* self);
+char* Program_getVersion(struct Program* self);
+void Program_printBanner(struct Program* self);
+void Program_setAuthor(struct Program* self, char* author);
+void Program_setName(struct Program* self, char* name);
+void Program_setVersion(struct Program* self, char* version);
 #endif
--- src/program.c
+++ src/program.c
@@ -3,38 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int Program_init(struct Program* program) {
-  program->getAuthor = &Program_getAuthor;
-  program->getName = &Program_getName;
-  program->getVersion = &Program_getVersion;
-  program->printBanner = &Program_printBanner;
-  program->setAuthor = &Program_setAuthor;
-  program->setName = &Program_setName;
-  program->setVersion = &Program_setVersion;
+int Program_init(struct Program* self) {
+  self->getAuthor = &Program_getAuthor;
+  self->getName = &Program_getName;
+  self->getVersion = &Program_getVersion;
+  self->printBanner = &Program_printBanner;
+  self->setAuthor = &Program_setAuthor;
+  self->setName = &Program_setName;
+  self->setVersion = &Program_setVersion;
   return 0;
 }
-char* Program_getAuthor(struct Program* program) {
-  return program->author;
+char* Program_getAuthor(struct Program* self) {
+  return self->author;
 }
-char* Program_getName(struct Program* program) {
-  return program->name;
+char* Program_getName(struct Program* self) {
+  return self->name;
 }
-char* Program_getVersion(struct Program* program) {
-  return program->version;
+char* Program_getVersion(struct Program* self) {
+  return self->version;
 }
-void Program_printBanner(struct Program* program) {
+void Program_printBanner(struct Program* self) {
   struct Banner banner;
-  Banner_init(&banner, program);
+  Banner_init(&banner, self);
   char* string = banner.getString(&banner);
   printf("%s", string);
   free(string);
 }
-void Program_setAuthor(struct Program* program, char* author) {
-  strcpy_s(program->author, sizeof program->author, author);
+void Program_setAuthor(struct Program* self, char* author) {
+  strcpy_s(self->author, sizeof self->author, author);
 }
-void Program_setName(struct Program* program, char* name) {
-  strcpy_s(program->name, sizeof program->name, name);
+void Program_setName(struct Program* self, char* name) {
+  strcpy_s(self->name, sizeof self->name, name);
 }
-void Program_setVersion(struct Program* program, char* version) {
-  strcpy_s(program->version, sizeof program->version, version);
+void Program_setVersion(struct Program* self, char* version) {
+  strcpy_s(self->version, sizeof self->version, version);
 }
