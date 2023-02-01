9ef1537a8898b1fad53c5086d68b7446c2b5d1a8
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-01-31 21:15:56 -0800

    Rename struct

--- include/banner.h
+++ include/banner.h
@@ -11,12 +11,12 @@ typedef struct {
   void (*setAuthorLine)(void* self, char* author);
   void (*setNameLine)(void* self, char* name);
   void (*setVersionLine)(void* self, char* version);
-} Banner;
-Banner* Banner_create(dgscan_program* program);
-void Banner_init(Banner* banner, dgscan_program* program);
-void Banner_destroy(Banner* banner);
-char* Banner_getString(Banner* self);
-void Banner_setAuthorLine(Banner* self, char* author);
-void Banner_setNameLine(Banner* self, char* name);
-void Banner_setVersionLine(Banner* self, char* version);
+} dgscan_banner;
+dgscan_banner* Banner_create(dgscan_program* program);
+void Banner_init(dgscan_banner* banner, dgscan_program* program);
+void Banner_destroy(dgscan_banner* banner);
+char* Banner_getString(dgscan_banner* self);
+void Banner_setAuthorLine(dgscan_banner* self, char* author);
+void Banner_setNameLine(dgscan_banner* self, char* name);
+void Banner_setVersionLine(dgscan_banner* self, char* version);
 #endif
--- src/banner.c
+++ src/banner.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-Banner* Banner_create(dgscan_program* program) {
-  Banner* banner = (Banner*) malloc(sizeof(Banner));
+dgscan_banner* Banner_create(dgscan_program* program) {
+  dgscan_banner* banner = (dgscan_banner*) malloc(sizeof(dgscan_banner));
   Banner_init(banner, program);
   return banner;
 }
-void Banner_init(Banner* banner, dgscan_program* program) {
+void Banner_init(dgscan_banner* banner, dgscan_program* program) {
   banner->getString = &Banner_getString;
   banner->setAuthorLine = &Banner_setAuthorLine;
   banner->setNameLine = &Banner_setNameLine;
@@ -17,14 +17,14 @@ void Banner_init(Banner* banner, dgscan_program* program) {
   banner->setVersionLine(banner, dgscan_program_get_version(program));
   banner->setAuthorLine(banner, dgscan_program_get_author(program));
 }
-void Banner_destroy(Banner* banner) {
+void Banner_destroy(dgscan_banner* banner) {
   if (banner) {
-    // Could call a void Banner_reset(Banner* banner)
+    // Could call a void Banner_reset(dgscan_banner* banner)
     // function here to unset vars used by the object.
     free(banner);
   }
 }
-char* Banner_getString(Banner* self) {
+char* Banner_getString(dgscan_banner* self) {
   int string_size = BANNER_LINE_SIZE * BANNER_LINE_COUNT;
   char* string = malloc(string_size);
   strncpy_s(string, string_size, "\n", BANNER_LINE_SIZE);
@@ -34,12 +34,12 @@ char* Banner_getString(Banner* self) {
   strncat_s(string, string_size, "\n", BANNER_LINE_SIZE);
   return string;
 }
-void Banner_setAuthorLine(Banner* self, char* author) {
+void Banner_setAuthorLine(dgscan_banner* self, char* author) {
   snprintf(self->author_line, sizeof self->author_line, "Author: %s\n", author);
 }
-void Banner_setNameLine(Banner* self, char* name) {
+void Banner_setNameLine(dgscan_banner* self, char* name) {
   snprintf(self->name_line, sizeof self->name_line, "%s\n", name);
 }
-void Banner_setVersionLine(Banner* self, char* version) {
+void Banner_setVersionLine(dgscan_banner* self, char* version) {
   snprintf(self->version_line, sizeof self->version_line, "Version: %s\n", version);
 }
