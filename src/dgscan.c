c5ffab3b64cd2f909a79eee98ba73714b3dfd4c1
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-02-01 19:42:49 -0800

    Add whitespace

--- src/dgscan.c
+++ src/dgscan.c
@@ -2,10 +2,12 @@
 #include "../include/program.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 #define DGSCAN_PROGRAM_AUTHOR "Daniel Gilbert"
 #define DGSCAN_PROGRAM_BINARY_NAME "dgscan"
 #define DGSCAN_PROGRAM_NAME "DG Scan"
 #define DGSCAN_PROGRAM_VERSION "1.0"
+
 int main(void) {
   dgscan_program* dgscan = dgscan_program_new();
   dgscan_main_init_program(dgscan);
@@ -14,6 +16,7 @@ int main(void) {
   dgscan_program_free(dgscan);
   return 0;
 }
+
 void dgscan_main_init_program(dgscan_program* program) {
   dgscan_program_set_author(program, DGSCAN_PROGRAM_AUTHOR);
   dgscan_program_set_binary_name(program, DGSCAN_PROGRAM_BINARY_NAME);
