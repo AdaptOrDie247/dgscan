e66a3a4a1e8623c0af3240718202596a1c6affeb
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-02-01 20:07:04 -0800

    Rename parameters

--- include/program.h
+++ include/program.h
@@ -9,16 +9,16 @@ typedef struct {
 } dgscan_program;
 
 void dgscan_program_free(dgscan_program* program);
-char* dgscan_program_get_author(dgscan_program* self);
-char* dgscan_program_get_binary_name(dgscan_program* self);
-char* dgscan_program_get_name(dgscan_program* self);
-char* dgscan_program_get_version(dgscan_program* self);
+char* dgscan_program_get_author(dgscan_program* program);
+char* dgscan_program_get_binary_name(dgscan_program* program);
+char* dgscan_program_get_name(dgscan_program* program);
+char* dgscan_program_get_version(dgscan_program* program);
 dgscan_program* dgscan_program_new();
-void dgscan_program_print_banner(dgscan_program* self);
-void dgscan_program_print_usage(dgscan_program* self);
-void dgscan_program_set_author(dgscan_program* self, char* author);
-void dgscan_program_set_binary_name(dgscan_program* self, char* binary_name);
-void dgscan_program_set_name(dgscan_program* self, char* name);
-void dgscan_program_set_version(dgscan_program* self, char* version);
+void dgscan_program_print_banner(dgscan_program* program);
+void dgscan_program_print_usage(dgscan_program* program);
+void dgscan_program_set_author(dgscan_program* program, char* author);
+void dgscan_program_set_binary_name(dgscan_program* program, char* binary_name);
+void dgscan_program_set_name(dgscan_program* program, char* name);
+void dgscan_program_set_version(dgscan_program* program, char* version);
 
 #endif
