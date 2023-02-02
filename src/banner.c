01d9cad4efdafc538837e774b0d4fccfc33a0230
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-02-01 19:50:32 -0800

    Add whitespace

--- src/banner.c
+++ src/banner.c
@@ -3,16 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 dgscan_banner* dgscan_banner_new(dgscan_program* program) {
   dgscan_banner* banner = (dgscan_banner*) malloc(sizeof(dgscan_banner));
   dgscan_banner_init(banner, program);
   return banner;
 }
+
 void dgscan_banner_init(dgscan_banner* banner, dgscan_program* program) {
   dgscan_banner_set_name_line(banner, dgscan_program_get_name(program));
   dgscan_banner_set_version_line(banner, dgscan_program_get_version(program));
   dgscan_banner_set_author_line(banner, dgscan_program_get_author(program));
 }
+
 void dgscan_banner_free(dgscan_banner* banner) {
   if (banner) {
     // Could call a void Banner_reset(dgscan_banner* banner)
@@ -20,6 +23,7 @@ void dgscan_banner_free(dgscan_banner* banner) {
     free(banner);
   }
 }
+
 char* dgscan_banner_get_string(dgscan_banner* self) {
   int string_size = DGSCAN_BANNER_LINE_SIZE * DGSCAN_BANNER_LINE_COUNT;
   char* string = malloc(string_size);
@@ -30,12 +34,15 @@ char* dgscan_banner_get_string(dgscan_banner* self) {
   strncat_s(string, string_size, "\n", DGSCAN_BANNER_LINE_SIZE);
   return string;
 }
+
 void dgscan_banner_set_author_line(dgscan_banner* self, char* author) {
   snprintf(self->author_line, sizeof self->author_line, "Author: %s\n", author);
 }
+
 void dgscan_banner_set_name_line(dgscan_banner* self, char* name) {
   snprintf(self->name_line, sizeof self->name_line, "%s\n", name);
 }
+
 void dgscan_banner_set_version_line(dgscan_banner* self, char* version) {
   snprintf(self->version_line, sizeof self->version_line, "Version: %s\n", version);
 }
