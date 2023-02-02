31a8ce7ef52d924741d6a9d5393f0db16186619e
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-02-01 19:48:46 -0800

    Add whitespace

--- include/program.h
+++ include/program.h
@@ -1,11 +1,13 @@
 #ifndef PROGRAM
 #define PROGRAM
+
 typedef struct {
   char author[40];
   char binary_name[20];
   char name[50];
   char version[20];
 } dgscan_program;
+
 dgscan_program* dgscan_program_new();
 void dgscan_program_free(dgscan_program* program);
 char* dgscan_program_get_author(dgscan_program* self);
@@ -18,4 +20,5 @@ void dgscan_program_set_author(dgscan_program* self, char* author);
 void dgscan_program_set_binary_name(dgscan_program* self, char* binary_name);
 void dgscan_program_set_name(dgscan_program* self, char* name);
 void dgscan_program_set_version(dgscan_program* self, char* version);
+
 #endif
